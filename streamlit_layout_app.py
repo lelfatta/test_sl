da792d7868aaf594e35e7a2f8b82508008d4ab13
lelfatta <lelfatta@gmail.com>
2023-09-11 19:41:34 -0700

    Update streamlit_layout_app.py

--- streamlit_layout_app.py
+++ streamlit_layout_app.py
@@ -161,7 +161,7 @@ def generate_final_answer(context, prompt):
     #print(response)
     return response
 #########################################################################################################################
-sample_movie = movies_df.sample(1)
+sample_movie = movies_df.sample(1).reset_index(drop=True)
 sample_company = companies_df.sample(1)
 sample_music = music_df.sample(1)
 
