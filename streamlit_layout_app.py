425777f3f753c62d9a3d0a876035863e6d557207
lelfatta <lelfatta@gmail.com>
2023-09-11 19:36:07 -0700

    Update streamlit_layout_app.py

--- streamlit_layout_app.py
+++ streamlit_layout_app.py
@@ -178,17 +178,28 @@ def main():
     col1, col2, col3 = st.columns(3)
     with col1:
         st.image("Romantisized golden era movie theater screen from a side angle lights dimmed.jfif", caption="Movies by Dalle2", use_column_width=True)
-        st.write("Sample Data:")
-        st.table(sample_movie)
+        
     with col2:
         st.image("Corporate buildings in Manhattan from below looking up_.jfif", caption="Corporations by Dalle2", use_column_width=True)
-        st.write("Sample Data:")
-        st.table(sample_company)
+        
     with col3:
         st.image("wide eyed lens shot vinyl record player in focus in the foreground background is a coffee shop but blurred with intense bokeh (1).jfif", caption="Music by Dalle2", use_column_width=True)
         st.write("Sample Data:")
         st.table(sample_music)
-    
+        
+    with st.expander("See movie sample data"):
+        st.write("Sample Data:")
+        st.table(sample_movie)
+
+    with st.expander("See company sample data"):
+        st.write("Sample Data:")
+        st.table(sample_company)
+
+    with st.expander("See music sample data"):
+        st.write("Sample Data:")
+        st.table(sample_music)
+             
+         
     # Show 'Clear Chat History' button only if chat history is not empty
     if st.session_state.chat_history:
         if st.button('Clear Chat History'):
